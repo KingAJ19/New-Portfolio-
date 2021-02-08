1ef102d207727d1143bea811d4ce14d1888c3d22
KingAJ19 <anelesokwane@gmail.com>
2021-02-08 09:45:26 +0200

    registered vue-typer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueTyperPlugin from 'vue-typer'
 import vuetify from './plugins/vuetify';
 import router from './router'
 import SuiVue from 'semantic-ui-vue';
 import 'semantic-ui-css/semantic.min.css';
 Vue.use(SuiVue);
+Vue.use(VueTyperPlugin)
 
 Vue.config.productionTip = false
 
