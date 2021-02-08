ad6b9b78c004c414057b05b69db6f2a5dd35e2bd
KingAJ19 <anelesokwane@gmail.com>
2021-02-08 11:06:35 +0200

    made the cards the same height

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,7 +3,7 @@
     <v-container class="my-6">
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="project in projects" :key="project.name" link :to="project.link">
-          <v-card text class="text-center ma-3">
+          <v-card height="95%" text class="text-center ma-3">
             <v-responsive class="pt-4">
               <v-avatar size="130"> 
                 <img :src="project.avatar">
