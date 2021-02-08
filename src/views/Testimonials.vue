f676736ff1c2fc3300d398b7925c8a8426ee5dd6
KingAJ19 <anelesokwane@gmail.com>
2021-02-08 11:08:06 +0200

    updated Asaph's Insta link

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -3,7 +3,7 @@
     <v-container class="my-6">
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="testimonial in testimonials" :key="testimonial.name">
-          <v-card text class="text-center ma-3">
+          <v-card height="95%" text class="text-center ma-3">
             <v-responsive class="pt-4">
               <v-avatar size="140"> 
                 <img :src="testimonial.avatar">
@@ -35,7 +35,7 @@
         { name: 'Jason Wandrag', message: 'Anele is an extremely creative and motivated person, who enjoys creating well scripted Youtube content. Anele has shown to bring this level of quality and creativity to his work, and never fails to bring a team together. He would be great for any marketing or development role.', avatar: './Jason.jpg', link: 'https://www.instagram.com/pixie_pandah' },
         { name: 'Oslin', message: 'Meeting Anele has added value to my life by listening to him and hearing what he have to say. He is such a creative person turning out quality content all the time, this makes him an even better developer because of his thought process.', avatar: '/Oslin.jpg', link: 'https://www.instagram.com/ossiekc' },
         { name: 'Kayleen Watkins', message: 'Anele is a very kind person  and is  a pleasure to be around.  He has proven through the course that he is a very dedicated person and is always improving his coding skills.', avatar: '/Kayleen.jpg', link: 'https://www.instagram.com/kayleenswatkins' },
-        { name: 'Asaph Franks', message: 'Anele is one word..."Driven". If he really wants to do something he will push himself to do it. He is also a super fun person to be around.', avatar: './Asaph.jpg', link: 'https://www.instagram.com/aj_the_ultimate' }
+        { name: 'Asaph Franks', message: 'Anele is one word..."Driven". If he really wants to do something he will push himself to do it. He is also a super fun person to be around.', avatar: './Asaph.jpg', link: 'https://www.instagram.com/aj_the_pianist' }
         
       ]
     }
