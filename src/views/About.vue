c0bf4c47615efdd98fd1ce5faf4e2cff7c62fa0a
KingAJ19 <anelesokwane@gmail.com>
2021-02-08 10:04:10 +0200

    added vuetyper to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,30 +11,28 @@
         >
           <div class="content">
             <h2 class="underline">Full Name</h2>
-            <vue-typer  erase-style='backspace' text='Anele Joshua Sokwane'></vue-typer>
+            <vue-typer  erase-style='select-back' text='Anele Joshua Sokwane'></vue-typer>
 
             <h2 class="underline">Date Of Birth</h2>
-            <p class="text">06/04/2002</p>
+            <vue-typer  erase-style='clear' text='06/04/2002'></vue-typer>
 
             <h2 class="underline">Gender</h2>
-            <p class="text">Male</p>
+            <vue-typer  erase-style='select-all' text='Male'></vue-typer>
 
             <h2 class="underline">Location</h2>
-            <p class="text">Eersteriver, Western Cape</p>
+            <vue-typer  erase-style='backspace' text='Eersteriver, Cape Town'></vue-typer>
 
             <h2 class="underline">Nationality</h2>
-            <p class="text">South African</p>
+            <vue-typer  erase-style='select-all' text='South African'></vue-typer>
 
             <h2 class="underline">Education</h2>
-            <p class="text">Tuscany Glen High School [Matric 2019]</p>
+            <vue-typer  erase-style='clear' text='Tuscany Glen High School [Matric 2019]'></vue-typer>
             <h2 class="underline">Hobbies</h2>
-            <p class="text">Basketball, 
-              Making videos, 
-              Drawing, 
-              Rapping</p>
+            
+            <vue-typer  erase-style='backspace' :text='["Basketball","Drawing","Rapping","Making Videos"]'></vue-typer>
             <h2 class="underline">Bio</h2>
-            <p class="text">I am very hard-working, passionate, committed and creative. I never give up no matter how hard things get because I believe that in order to succeed, you have to fail and learn from that failure to get better. I also believe that friends and family are important but make sure that the relationships are good.
-            Ever sinceI was young, I always loved computers and working with them. Everyone would come to me whenever they had a problem with their computers. I got interested in coding once my cousin told me about it and I saw the world differently because I realised that most of the most successful websites and pages that we use on a daily basis are made with ...words. It has always been fascinating to me. I enjoy coding and I try to code something each and every day. I am very dedicated to getting better and developing my skills.</p>
+            <vue-typer  erase-style='clear' text='I am very hard-working, passionate, committed and creative. I never give up no matter how hard things get because I believe that in order to succeed, you have to fail and learn from that failure to get better. I also believe that friends and family are important but make sure that the relationships are good.
+            Ever sinceI was young, I always loved computers and working with them. Everyone would come to me whenever they had a problem with their computers. I got interested in coding once my cousin told me about it and I saw the world differently because I realised that most of the most successful websites and pages that we use on a daily basis are made with ...words. It has always been fascinating to me. I enjoy coding and I try to code something each and every day. I am very dedicated to getting better and developing my skills.'></vue-typer>
           
           </div>
          
