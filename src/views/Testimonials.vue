aabec00808835a9bfe383ed5b27005f4f94d9d24
KingAJ19 <anelesokwane@gmail.com>
2021-02-09 13:31:38 +0200

    finished Elan's testimonial

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -36,8 +36,8 @@
         { name: 'Oslin', message: 'Meeting Anele has added value to my life by listening to him and hearing what he have to say. He is such a creative person turning out quality content all the time, this makes him an even better developer because of his thought process.', avatar: '/Oslin.jpg', link: 'https://www.instagram.com/ossiekc' },
         { name: 'Kayleen Watkins', message: 'Anele is a very kind person  and is  a pleasure to be around.  He has proven through the course that he is a very dedicated person and is always improving his coding skills.', avatar: '/Kayleen.jpg', link: 'https://www.instagram.com/kayleenswatkins' },
         { name: 'Asaph Franks', message: 'Anele is one word..."Driven". If he really wants to do something he will push himself to do it. He is also a super fun person to be around.', avatar: './Asaph.jpg', link: 'https://www.instagram.com/aj_the_pianist' },
-        { name: 'Navan Basson', message: 'Anele has many amazing qualities. He is creative, hardworking, passionate, works well under pressure and very reliable.', avatar: '/Navan.jpg', link: 'https://www.instagram.com/navan_jean_basson'},
-        { name: 'Elan Abrahams', message: 'Anele Sokwane is one of the hardest people I know. Pause', avatar: '/Elan.jpg', link: 'https://www.instagram.com/elanabrahams'}
+        { name: 'Navan Basson', message: 'Anele has many amazing qualities. He is creative, hard-working, passionate, works well under pressure and very reliable.', avatar: '/Navan.jpg', link: 'https://www.instagram.com/navan_jean_basson'},
+        { name: 'Elan Abrahams', message: 'Anele is a hard-worker and one of his greatest strengths is his creativity. He is able to find notable solutions and new ideas easily.', avatar: '/Elan.jpg', link: 'https://www.instagram.com/elanabrahams'}
         
       ]
     }
