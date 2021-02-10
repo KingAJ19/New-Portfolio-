2c7ecd62caf2e352fbdcc84f5e8dad4352c2a71e
KingAJ19 <anelesokwane@gmail.com>
2021-02-10 12:45:42 +0200

    made a vue card in the stack page

--- src/views/Stack.vue
+++ src/views/Stack.vue
@@ -1,6 +1,62 @@
 <template>
 <div class="stack page">
-  <h2>Stack Page</h2>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex>
+        <v-col 
+          xs="12" 
+          sm="6" 
+          md="3"
+          lg="4"
+        >
+        <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <v-img src="/vue.png"
+              height="225"
+              class="grey darken-4">
+      </v-img>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            VueJS
+          </p>
+          <h3>I've been using Vue for the past 5 months. </h3>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="reveal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
+        </v-col>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </div>
 </template>
 
