5aef75bf7edcd2fc06ae1b9fd1f337f849555849
KingAJ19 <anelesokwane@gmail.com>
2021-02-10 14:27:39 +0200

    got the cards to be responsive

--- src/views/Stack.vue
+++ src/views/Stack.vue
@@ -1,15 +1,10 @@
 <template>
 <div class="stack page">
-  <v-container>
-    <v-layout row wrap>
-      <v-flex>
-        <v-col 
-          xs="12" 
-          sm="6" 
-          md="3"
-          lg="4"
-        >
-        <v-card
+  <br>
+  <br>
+  <v-row>
+    <v-col cols="12" md="6" sm="6" xs="9">
+      <v-card
     class="mx-auto"
     max-width="344"
   >
@@ -53,10 +48,190 @@
       </v-card>
     </v-expand-transition>
   </v-card>
-        </v-col>
-      </v-flex>
-    </v-layout>
-  </v-container>
+
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <v-img src="/vue.png"
+              height="225"
+              class="grey darken-4">
+      </v-img>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn
+          color="primary"
+          dark
+          @click="dialog = true"
+        >
+          Learn More
+        </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="dialog === true"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            VueJS
+          </p>
+          <h3>I've been using Vue for the past 5 months. </h3>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
+    </v-col>
+    <v-col cols="12" md="6" sm="6" xs="9">
+      <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <v-img src="/vue.png"
+              height="225"
+              class="grey darken-4">
+      </v-img>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn
+          color="primary"
+          dark
+          @click="dialog = true"
+        >
+          Learn More
+        </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="dialog === true"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            VueJS
+          </p>
+          <h3>I've been using Vue for the past 5 months. </h3>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
+  
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <v-img src="/vue.png"
+              height="225"
+              class="grey darken-4">
+      </v-img>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn
+          color="primary"
+          dark
+          @click="dialog = true"
+        >
+          Learn More
+        </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="dialog === true"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            VueJS
+          </p>
+          <h3>I've been using Vue for the past 5 months. </h3>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
+    </v-col>
+  </v-row>
+<!--        
+        <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <v-img src="/vue.png"
+              height="225"
+              class="grey darken-4">
+      </v-img>
+    </v-card-text>
+    <v-card-actions>
+        <v-btn
+          color="primary"
+          dark
+          @click="dialog = true"
+        >
+          Learn More
+        </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="dialog === true"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="display-1 text--primary">
+            VueJS
+          </p>
+          <h3>I've been using Vue for the past 5 months. </h3>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card> -->
+  <!-- </v-col> -->
 </div>
 </template>
 
