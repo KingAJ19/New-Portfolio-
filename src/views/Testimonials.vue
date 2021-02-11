6d643dc5aaa0d9b924ce4f5ef6dbd8f277a5ba14
KingAJ19 <anelesokwane@gmail.com>
2021-02-11 13:47:50 +0200

    added animation for the testimonials

--- src/views/Testimonials.vue
+++ src/views/Testimonials.vue
@@ -3,7 +3,7 @@
     <v-container class="my-6">   
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 lg3 v-for="testimonial in testimonials" :key="testimonial.name">
-          <v-card height="95%" text class="text-center ma-3">
+          <v-card height="95%" text class="text-center ma-3 animate__animated animate__flipInY">
             <v-responsive class="pt-4">
               <v-avatar size="140"> 
                 <img :src="testimonial.avatar">
