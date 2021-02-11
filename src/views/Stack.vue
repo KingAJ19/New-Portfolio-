978b865755bf42c366922f38fb9acac58a966882
KingAJ19 <anelesokwane@gmail.com>
2021-02-11 16:24:26 +0200

    done with stack page

--- src/views/Stack.vue
+++ src/views/Stack.vue
@@ -33,7 +33,7 @@
           <p class="display-1" id="vue">
             Vue
           </p>
-          <h3>I've been using Vue for the past 5 months. It has become one of my favourite Javascript frameworks to use and it was easy to learn.</h3>
+          <h3>I've been using Vue for the past 5 months. It has become one of my favourite Javascript frameworks to use and it was easy to learn. The two-way binding is one of my favourite features of Vue.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
@@ -75,10 +75,10 @@
         style="height: 100%;"
       >
         <v-card-text class="pb-0">
-          <p class="display-1 text--primary">
+          <p class="display-1" id="python">
             Python
           </p>
-          <h3>I've been using Python for the past 5 months. </h3>
+          <h3>Python is the first programming language that I've learnt. I did a lot of Python projects in order to understand it better. The syntax is English-like, so that made it easier to read and write code.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
@@ -121,10 +121,10 @@
         style="height: 100%;"
       >
         <v-card-text class="pb-0">
-          <p class="display-1 text--primary">
+          <p class="display-1" id="html">
             HTML
           </p>
-          <h3>I've been using HTML for the past 5 months. </h3>
+          <h3>HTML is the first language I learnt. It was extremely easy to learn because it's just plain text. I got much better at understanding how to structure webpages and layouts after practicing it for so long.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
@@ -166,10 +166,10 @@
         style="height: 100%;"
       >
         <v-card-text class="pb-0">
-          <p class="display-1 text--primary">
+          <p class="display-1" id="css">
             CSS
           </p>
-          <h3>I've been using CSS for the past 5 months. </h3>
+          <h3>I had to learn CSS at the same time that I learnt HTML. At first I was always confused about classes, id's and media queries but now it's like second nature to me.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
@@ -213,10 +213,10 @@
         style="height: 100%;"
       >
         <v-card-text class="pb-0">
-          <p class="display-1 text--primary">
+          <p class="display-1" id="js">
             Javascript
           </p>
-          <h3>I've been using Javascript for the past 5 months. </h3>
+          <h3>It took me a really long time to understand the syntax and how to create functions. I mostly use frameworks so the Javascript is already done for me but I'm able to write and understand Javascript code.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn
@@ -261,7 +261,7 @@
           <p class="display-1 text--primary">
             React
           </p>
-          <h3>I've been using React for the past 5 months. </h3>
+          <h3>React was the first framework that I used. The fact that it uses JSX made it that much easier to learn. React is very flexible and easily maintainable. I hardly use React anymore ever since I discovered Vue.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn 
@@ -306,10 +306,10 @@
         style="height: 100%;"
       >
         <v-card-text class="pb-0">
-          <p class="display-1 text--primary">
+          <p class="display-1" id="node">
             Node
           </p>
-          <h3>I've been using Node for the past 5 months. </h3>
+          <h3>I rarely use Node since I prefer the frontend but when I do - I use it for it's package management and whenever I need to set up and express server or make HTTP requests.</h3>
         </v-card-text>
         <v-card-actions class="pt-0">
           <v-btn 
@@ -354,5 +354,17 @@
 }
 #vue{
   color: teal;
+}
+#python{
+  color: #FFE873;
+}
+#html{
+  color: #fb8c00;
+}
+#css{
+  color: #2196f3;
+}
+#node{
+  color: #3c873a;
 }
 </style>
